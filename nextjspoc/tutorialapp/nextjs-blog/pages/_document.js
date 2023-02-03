3174b9bb761200e779f2b529fea7ade8ce4c3e6d
Matthew Brown <matthew.brown1@nhs.net>
2023-02-03 17:30:37 +0000

    fix links

--- nextjspoc/tutorialapp/nextjs-blog/pages/_document.js
+++ nextjspoc/tutorialapp/nextjs-blog/pages/_document.js
@@ -23,7 +23,7 @@ export default function Document() {
         <header className="nhsuk-header" role="banner">
           <div className="nhsuk-width-container nhsuk-header__container">
             <div className="nhsuk-header__logo nhsuk-header__logo--only">
-              <Link className="nhsuk-header__link nhsuk-header__link--service " href="/index" aria-label="NHS homepage">
+              <Link className="nhsuk-header__link nhsuk-header__link--service " href="/" aria-label="NHS homepage">
                 <svg className="nhsuk-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 16" height="40" width="100">
                   <path className="nhsuk-logo__background" fill="#005eb8" d="M0 0h40v16H0z"></path>
                   <path className="nhsuk-logo__text" fill="#fff" d="M3.9 1.5h4.4l2.6 9h.1l1.8-9h3.3l-2.8 13H9l-2.7-9h-.1l-1.8 9H1.1M17.3 1.5h3.6l-1 4.9h4L25 1.5h3.5l-2.7 13h-3.5l1.1-5.6h-4.1l-1.2 5.6h-3.4M37.7 4.4c-.7-.3-1.6-.6-2.9-.6-1.4 0-2.5.2-2.5 1.3 0 1.8 5.1 1.2 5.1 5.1 0 3.6-3.3 4.5-6.4 4.5-1.3 0-2.9-.3-4-.7l.8-2.7c.7.4 2.1.7 3.2.7s2.8-.2 2.8-1.5c0-2.1-5.1-1.3-5.1-5 0-3.4 2.9-4.4 5.8-4.4 1.6 0 3.1.2 4 .6"></path>
@@ -40,9 +40,9 @@ export default function Document() {
         <nav className="nhsuk-breadcrumb" aria-label="Breadcrumb">
           <div className="nhsuk-width-container">
             <ol className="nhsuk-breadcrumb__list">
-              <li className="nhsuk-breadcrumb__item"><Link href="/index" className="nhsuk-breadcrumb__link">Home</Link></li>
+              <li className="nhsuk-breadcrumb__item"><Link href="/" className="nhsuk-breadcrumb__link">Home</Link></li>
             </ol>
-            <p className="nhsuk-breadcrumb__back"><Link className="nhsuk-breadcrumb__backlink" href="/index">Back to Home</Link></p>
+            <p className="nhsuk-breadcrumb__back"><Link className="nhsuk-breadcrumb__backlink" href="/">Back to Home</Link></p>
           </div>
         </nav>
         <div className="nhsuk-width-container ">
