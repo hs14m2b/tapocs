b37c3184b369a7d0a429d1bf959b9db09799ce91
Matthew Brown <23528562+hs14m2b@users.noreply.github.com>
2023-02-27 18:55:07 +0000

    dynamic url rewrite to static url with path param (#7)

--- nextjspoc/tutorialapp/infrastructure/build.sh
+++ nextjspoc/tutorialapp/infrastructure/build.sh
@@ -2,6 +2,7 @@
 
 ENVIRONMENT="nhsukpocbe"
 S3CODEBUCKET="codepipeline-eu-west-2-467564981221"
+USS3CODEBUCKET="lambdacodenextjspocedge"
 #OPENSSL_CONF=/dev/null
 TIMESTAMP=$(date +%s)
 echo $TIMESTAMP
@@ -27,15 +28,25 @@ cp ../../lambdas/${TIMESTAMP}nextjspocapilambdas.zip ${TIMESTAMP}nextjspocapilam
 rm ../../lambdas/${TIMESTAMP}nextjspocapilambdas.zip
 # update cloudformation templates
 cp ../../infrastructure/nextjspoc-backend.json nextjspoc-backend.json
+cp ../../infrastructure/nextjspoc-edge.json nextjspoc-edge.json
 cp ../../infrastructure/nextjspoc-frontend.json ${TIMESTAMP}nextjspoc-frontend.json
 sed -i "s/code\.zip/${TIMESTAMP}code\.zip/g" nextjspoc-backend.json
 sed -i "s/dependenciesLayer\.zip/${TIMESTAMP}dependenciesLayer\.zip/g" nextjspoc-backend.json
 sed -i "s/nextjspocapilambdas\.zip/${TIMESTAMP}nextjspocapilambdas\.zip/g" nextjspoc-backend.json
+sed -i "s/nextjspocapilambdas\.zip/${TIMESTAMP}nextjspocapilambdas\.zip/g" nextjspoc-edge.json
+sed -i "s/BLDTIME/${TIMESTAMP}/g" nextjspoc-edge.json
 
+#backend
 aws cloudformation package --use-json --template-file nextjspoc-backend.json --s3-bucket ${S3CODEBUCKET} --output-template-file ${TIMESTAMP}nextjspoc-backend.json
 aws cloudformation deploy --template-file ${TIMESTAMP}nextjspoc-backend.json --stack-name main-mabr8-nextjspocbestack --capabilities "CAPABILITY_NAMED_IAM"
-#aws cloudformation create-stack --template-body file://${TIMESTAMP}nhsukpoc-backend.json --stack-name main-mabr8-nextjspocbestack --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
-aws cloudformation deploy --template-file ${TIMESTAMP}nextjspoc-frontend.json --stack-name main-mabr8-nextjspocfestack --capabilities "CAPABILITY_NAMED_IAM"
+#lambda at edge
+#aws cloudformation package --use-json --template-file nextjspoc-edge.json --s3-bucket ${USS3CODEBUCKET} --output-template-file ${TIMESTAMP}nextjspoc-edge.json
+#aws cloudformation deploy --template-file ${TIMESTAMP}nextjspoc-edge.json --stack-name main-mabr8-nextjspocedgestack --capabilities "CAPABILITY_NAMED_IAM" --region "us-east-1"
+#frontend
+#aws cloudformation deploy --template-file ${TIMESTAMP}nextjspoc-frontend.json --stack-name main-mabr8-nextjspocfestack --capabilities "CAPABILITY_NAMED_IAM"
+
+##aws cloudformation create-stack --template-body file://${TIMESTAMP}nhsukpoc-backend.json --stack-name main-mabr8-nextjspocbestack --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
+
 
 mkdir assets
 unzip ${TIMESTAMP}assetsLayer.zip -d assets
