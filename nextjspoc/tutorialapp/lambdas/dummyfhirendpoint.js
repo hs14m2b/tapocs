d5eeb8cf0e8aaf79a2354d36cd5d43280911fcd9
matthew <matthew.brown1@nhs.net>
2023-11-21 16:51:35 +0000

    dummy FHIR endpoint

--- nextjspoc/tutorialapp/lambdas/dummyfhirendpoint.js
+++ nextjspoc/tutorialapp/lambdas/dummyfhirendpoint.js
@@ -2,12 +2,48 @@ exports.handler = async (event) => {
     console.log(JSON.stringify(event));
     try {
         console.log(event.body);
+        
+        const plain = Buffer.from(event.body, 'base64').toString('utf8');
+        const requestJson = JSON.parse(plain);
+        console.log(JSON.stringify(requestJson));
+
+        //find identifier for DocumentReference
+        let DRID = "";
+        let DMID = "";
+        for (let entry of requestJson.entry) {
+            if (entry.resource.resourceType == "DocumentReference") DRID = entry.resource.masterIdentifier.value.replace("urn:oid:", "");
+            if (entry.resource.resourceType == "DocumentManifest") DMID = entry.resource.masterIdentifier.value.replace("urn:oid:", "");
+        }
+
+        let hardcodedresponse = {
+            "resourceType": "Bundle",
+            "type": "transaction-response",
+            "entry": [ {
+              "response": {
+                "status": "201",
+                "location": "https://main-nextjsfe.nhsdta.com/extapi/FHIR/R4/dummyfhirendpoint/DocumentReference/" + DRID
+              }
+            }, {
+              "response": {
+                "status": "201",
+                "location": "https://main-nextjsfe.nhsdta.com/extapi/FHIR/R4/dummyfhirendpoint/DocumentManifest/" + DMID
+              }
+            }, {
+              "response": {
+                "status": "201",
+                "location": "https://main-nextjsfe.nhsdta.com/extapi/FHIR/R4/dummyfhirendpoint/Binary/" + DRID
+              }
+            } ]
+          };
+          
+
+
         let response = {
             statusCode: 200,
             "headers": {
-                "Content-Type": "application/json"
+                "Content-Type": "application/fhir+json"
             },
-            body: JSON.stringify({"result": "OK"})
+            body: JSON.stringify(hardcodedresponse)
         };
         console.log(JSON.stringify(response));
         return response;
