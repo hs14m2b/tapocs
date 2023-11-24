cb26eb4ad59f066cca439ba3de0fa5866086a1ac
matthew <matthew.brown1@nhs.net>
2023-11-24 14:38:49 +0000

    prep for invoking NRL API

---
+++ mhdpoc/lambdas/test/find_document_ref_sandpit.mjs
@@ -0,0 +1,73 @@
+import { URLSearchParams } from 'url';
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
+const { sign } = jwt;
+const HTTPS = "https://";
+
+let https;
+try {
+  https = await import('node:https');
+} catch (err) {
+  console.log('https support is disabled!');
+}
+let newId = uuidv4();
+let nhsNumber = "4409815415";
+let nhsNumberSystem = "https://fhir.nhs.uk/Id/nhs-number";
+
+ 
+async function findDocRef (nhsNumber) 
+  {
+    let XRequestID = uuidv4();
+    let queryParams = new URLSearchParams({
+      "subject:identifier": nhsNumberSystem + "|" + nhsNumber
+    }).toString();
+    // request option
+    let options = {
+      host: "sandbox.api.service.nhs.uk",
+      port: 443,
+      method: 'GET',
+      path: "/record-locator/producer/FHIR/R4/DocumentReference?" + queryParams,
+      rejectUnauthorized: false,
+      headers: {
+        //'Authorization': 'Bearer '+ access_token,
+        'accept': 'application/fhir+json;version=1',
+        'X-Request-ID': XRequestID,
+        'NHSD-End-User-Organisation-ODS': 'Y05868'
+      }
+    };
+  
+    console.log("request options are  " + JSON.stringify(options));
+    return new Promise(function (resolve, reject) {
+        // request object
+        var req = https.request(options, function (res) {
+            var result = '';
+            console.log("HTTP status code: " + res.statusCode);
+            res.on('data', function (chunk) {
+                result += chunk;
+            });
+            res.on('end', function () {
+                console.log(result);
+                let response = {
+                  "status": res.statusCode,
+                  "body": result
+                }
+                resolve(response);
+            });
+            res.on('error', function (err) {
+                console.log(err);
+                reject(err);
+            })
+        });
+         
+        // req error
+        req.on('error', function (err) {
+          console.log(err);
+        });
+         
+        //send request
+        req.end();
+    });
+  }
+
+let result = await findDocRef(nhsNumber);
+console.log(result);
---
+++ mhdpoc/lambdas/test/test_api_common_functions.mjs
@@ -0,0 +1,14 @@
+import { createSignedJwtForAuth, getOAuth2AccessToken } from '../api_common_functions.mjs';
+
+import { createPrivateKey } from 'node:crypto';
+import { readFileSync } from 'node:fs';
+
+const apiClientPrivateKey = readFileSync('../certs/mhdconvint1.key', 'utf8');
+//console.log(apiClientPrivateKey);
+//let secretOrPrivateKey = createPrivateKey(apiClientPrivateKey);
+let blah = await createSignedJwtForAuth("kqU7ldmK4wVoDQA6c76bNsAFMzw8SmGQ", 
+"mhdconvint1", apiClientPrivateKey,
+"int.api.service.nhs.uk", "/oauth2/token");
+console.log(blah);
+let blah2 = await getOAuth2AccessToken(blah, "int.api.service.nhs.uk", "/oauth2/token");
+console.log(blah2);
