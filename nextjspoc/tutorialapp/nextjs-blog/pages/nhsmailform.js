ccde39d229a8f49efde3f40a5da8018a6b357a08
Matthew Brown <matthew.brown1@nhs.net>
2024-03-07 11:39:12 +0000

    NHSmail Auth foundation

---
+++ nextjspoc/tutorialapp/nextjs-blog/pages/nhsmailform.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Link from 'next/link';
+const FORMDATACOOKIENAME = "formdata";
+
+export default function Home(props) {
+  function clearData() {
+    document.cookie = FORMDATACOOKIENAME + "=; path=/";
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+  return (
+    <>
+      <Head>
+        <title>Dynamic UI Framework PoC</title>
+        <link rel="icon" href="/assets/favicons/favicon.ico" />
+      </Head>
+      <h2 className="nhsuk-heading-l">
+          Next.js app running in AWS Lambda {props.prop1}
+      </h2>
+      <p>
+        <a href='https://blah'>Launch NHSmail authentication</a>
+      </p>
+
+
+    </>
+  )
+}
+
+export async function getServerSideProps(ctx) 
+{
+  if (ctx.req) {
+    console.log("running on server");
+    console.log(ctx.req.method);
+  }
+  return({props: {
+    "prop1": "blah"
+  }})
+}
