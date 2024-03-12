793e33a3973ed4de235331fdac70c5eb77ed5929
Matthew Brown <matthew.brown1@nhs.net>
2024-03-12 10:48:41 +0000

    new datastore

--- healthlake/scripts/createDocRef.sh
+++ healthlake/scripts/createDocRef.sh
@@ -1,4 +1,4 @@
-curl -X POST https://healthlake.us-east-1.amazonaws.com/datastore/7b4ecba72dc7fef0b302931ab19d5f32/r4/DocumentReference \
+curl -X POST https://healthlake.us-east-1.amazonaws.com/datastore/22a0fb0dbfb52c17ca716f0b92c64d11/r4/DocumentReference \
     -H "Content-Type: application/fhir+json" \
     -H "Authorization: Bearer THISISTHETOKEN" \
     --data "@docref.json"
--- healthlake/scripts/searchDocRef.sh
+++ healthlake/scripts/searchDocRef.sh
@@ -1,3 +1,3 @@
-curl -X GET "https://healthlake.us-east-1.amazonaws.com/datastore/7b4ecba72dc7fef0b302931ab19d5f32/r4/DocumentReference" \
+curl -X GET "https://healthlake.us-east-1.amazonaws.com/datastore/22a0fb0dbfb52c17ca716f0b92c64d11/r4/DocumentReference" \
     -G --data-urlencode 'subject%3Aidentifier=https://fhir.nhs.uk/Id/nhs-number|6700028191' \
     -H "Authorization: Bearer THISISTHETOKEN" --verbose
