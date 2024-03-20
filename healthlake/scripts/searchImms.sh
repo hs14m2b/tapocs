6c014cf46bba4887b2a137b641eae580e3e23f44
Matthew Brown <matthew.brown1@nhs.net>
2024-03-20 20:21:18 +0000

    search params

--- healthlake/scripts/searchImms.sh
+++ healthlake/scripts/searchImms.sh
@@ -3,8 +3,8 @@
 . ./setHealthlakeEndpoint.sh
 
 curl -X GET "${HEALTHLAKEENDPOINT}Immunization" \
-    -G --data-urlencode 'patient.identifier=https://fhir.nhs.uk/Id/nhs-number|6700028191' \
-    -G --data-urlencode 'vaccine-code=http://snomed.info/sct|39114911000001105' \
-    -H "Authorization: Bearer THISISTHETOKEN"
+    -G --data-urlencode 'patient%3Aidentifier=https://fhir.nhs.uk/Id/nhs-number|slkfsdlkj' \
+    -H "Authorization: Bearer THISISTHETOKEN" --verbose
 
+#    -G --data-urlencode 'vaccine-code=http://snomed.info/sct|39114911000001105' \
 
