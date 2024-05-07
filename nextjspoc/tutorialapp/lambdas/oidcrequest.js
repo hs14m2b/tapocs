85d85f735b15375529702ab3c840af6e66db0890
Matthew Brown <23528562+hs14m2b@users.noreply.github.com>
2024-05-07 20:49:46 +0100

    Nhsmailentraoidc (#25)
    * set up oidc client
    
    * dynamic prompt value
    
    * amr claims parameter
    
    * uplift
    
    * working

---
+++ nextjspoc/tutorialapp/lambdas/oidcrequest.js
@@ -0,0 +1,47 @@
+exports.handler = async (event) => {
+    console.log(JSON.stringify(event));
+
+    try {
+        let redirect_uri = (event.queryStringParameters && event.queryStringParameters.redirect_uri && event.queryStringParameters.redirect_uri.length > 0) ? event.queryStringParameters.redirect_uri: false;
+        let state = (event.queryStringParameters && event.queryStringParameters.state && event.queryStringParameters.state.length > 0) ? event.queryStringParameters.state: false;
+        let prompt = (event.queryStringParameters && event.queryStringParameters.prompt && event.queryStringParameters.prompt.length > 0) ? event.queryStringParameters.prompt: false;
+        let endpoint = (event.queryStringParameters && event.queryStringParameters.endpoint && event.queryStringParameters.endpoint.length > 0) ? event.queryStringParameters.endpoint: "v2";
+        let authorization_endpoint= (endpoint == "v2") ? "https://login.microsoftonline.com/37c354b2-85b0-47f5-b222-07b48d774ee3/oauth2/v2.0/authorize" : "https://login.microsoftonline.com/37c354b2-85b0-47f5-b222-07b48d774ee3/oauth2/authorize";
+        let qsvalues = {
+            client_id: "66dcf442-2933-4d84-b50d-30c7404632b6",
+            redirect_uri: (redirect_uri) ? redirect_uri: "https://main-nextjsfe.nhsdta.com/extapi/oidcresponse",
+            state: (state) ? state: "tempstate",
+            scope: "openid profile email phone",
+            response_type: "code",
+            claims: JSON.stringify({
+                "id_token" : {
+                    "auth_time" :  { "essential" : true }
+                    //"amr" : { "essential" : true, "values": ["mfa"] }
+                }
+            })
+        }
+        qsvalues['state'] = qsvalues['state'] + "|" + endpoint;
+        if (prompt) qsvalues['prompt'] = prompt;
+        let searchparams = new URLSearchParams(qsvalues).toString();
+        let response = {
+            "statusCode": 302,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Location": authorization_endpoint + "?" + searchparams
+            }
+        };
+        console.log(JSON.stringify(response));
+        return response;
+
+    } catch (error) {
+        console.log("caught error " + error.message);
+        let response = {
+            statusCode: 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({"error": error.message})
+        }
+        return response;
+    }
+}
--- nextjspoc/tutorialapp/nextjs-blog/pages/nhsmailform.js
+++ nextjspoc/tutorialapp/nextjs-blog/pages/nhsmailform.js
@@ -1,24 +1,27 @@
 import Head from 'next/head';
-import Link from 'next/link';
 const FORMDATACOOKIENAME = "formdata";
 
-export default function Home(props) {
-  function clearData() {
-    document.cookie = FORMDATACOOKIENAME + "=; path=/";
-    localStorage.clear();
-    sessionStorage.clear();
-  }
+function Home(props) {
   return (
     <>
       <Head>
-        <title>Dynamic UI Framework PoC</title>
+        <title>Dynamic UI Framework PoC rendered on { props.execlocation }</title>
         <link rel="icon" href="/assets/favicons/favicon.ico" />
       </Head>
       <h2 className="nhsuk-heading-l">
           Next.js app running in AWS Lambda {props.prop1}
       </h2>
       <p>
-        <a href='https://blah'>Launch NHSmail authentication</a>
+        <a href='/extapi/oidcrequest'>Launch NHSmail authentication (v2 endpoint) (no amr claim available)</a>
+      </p>
+      <p>
+        <a href='/extapi/oidcrequest?prompt=login'>Launch NHSmail authentication forcing re-authentication (v2 endpoint) (no amr claim available)</a>
+      </p>
+      <p>
+        <a href='/extapi/oidcrequest?endpoint=v1'>Launch NHSmail authentication (v1 endpoint) (amr claim available)</a>
+      </p>
+      <p>
+        <a href='/extapi/oidcrequest?prompt=login&endpoint=v1'>Launch NHSmail authentication forcing re-authentication (v1 endpoint) (amr claim available)</a>
       </p>
 
 
@@ -26,13 +29,24 @@ export default function Home(props) {
   )
 }
 
-export async function getServerSideProps(ctx) 
-{
+Home.getInitialProps = async (ctx) => {
+  console.log("in initial props");
+  let props = {
+    "execlocation": "server",
+    "givenname": "",
+    "familyname": "",
+    "gnerror": false,
+    "fnerror": false,
+    "prop1": "blah"
+  };
   if (ctx.req) {
     console.log("running on server");
     console.log(ctx.req.method);
   }
-  return({props: {
-    "prop1": "blah"
-  }})
+  else {
+    props["execlocation"] = "client";
+  }
+  return props;
 }
+
+export default Home;
