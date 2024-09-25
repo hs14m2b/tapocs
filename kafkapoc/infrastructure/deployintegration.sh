f045d6e66b6db93aa4cfa18b6da2abda8c812e0b
Matthew Brown <matthew.brown1@nhs.net>
2024-09-25 15:35:01 +0100

    working streaming

--- kafkapoc/infrastructure/deployintegration.sh
+++ kafkapoc/infrastructure/deployintegration.sh
@@ -11,13 +11,26 @@ S3CODEBUCKET="codepipeline-eu-west-2-467564981221"
 #OPENSSL_CONF=/dev/null
 TIMESTAMP=$(date +%s)
 echo $TIMESTAMP
+REGION="eu-west-2"
 
+# build lambda code
 cd ..
 mkdir build
+cd lambdas
+npm install
+zip -qr ${TIMESTAMP}firehosetransformlambdas.zip ./*
+cd ..
 cd build
+cp ../lambdas/${TIMESTAMP}firehosetransformlambdas.zip ${TIMESTAMP}firehosetransformlambdas.zip
+rm ../lambdas/${TIMESTAMP}firehosetransformlambdas.zip
 # update cloudformation templates
 cp ../infrastructure/kafkaintegrationcomponents.json kafkaintegrationcomponents.json
-aws cloudformation deploy --template-file kafkaintegrationcomponents.json --stack-name ${ENVIRONMENT}-mabr8-kafkapocintcomps --capabilities "CAPABILITY_NAMED_IAM" --region "eu-west-2" --parameter-overrides ${PARAMETERS}
+sed -i "s/firehosetransformlambdas\.zip/${TIMESTAMP}firehosetransformlambdas\.zip/g" kafkaintegrationcomponents.json
+
+#backend
+aws cloudformation package --use-json --template-file kafkaintegrationcomponents.json --s3-bucket ${S3CODEBUCKET} --output-template-file ${TIMESTAMP}kafkaintegrationcomponents.json --region ${REGION}
+aws cloudformation deploy --template-file ${TIMESTAMP}kafkaintegrationcomponents.json --stack-name ${ENVIRONMENT}-mabr8-kafkapocintcomps --capabilities "CAPABILITY_NAMED_IAM" --region ${REGION} --parameter-overrides ${PARAMETERS}
+
 
 cd ..
 rm -fR build
