a2dad942a301f124e8f24a62a98f529a90db53b3
Matthew Brown <matthew.brown1@nhs.net>
2024-10-02 11:48:02 +0100

    deploy in eu-west-2 successful

---
+++ healthlake/infrastructure/createdatastore-useast1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+aws healthlake create-fhir-datastore  --region us-east-1 --cli-input-json file://healthlake-datastore-useast1.json
+
--- healthlake/infrastructure/deletedatastore.sh
+++ healthlake/infrastructure/deletedatastore.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-aws healthlake delete-fhir-datastore  --region us-east-1 --datastore-id bbeafea565992204fdbdd324cb458824
+aws healthlake delete-fhir-datastore  --region eu-west-2 --datastore-id 1471d297aa2de3ed14c2dd98ead006e5
 
