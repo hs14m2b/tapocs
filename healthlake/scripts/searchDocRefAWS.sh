1e306f599edc1f9c8a03aa70b0fe52dda46a5421
Matthew Brown <matthew.brown1@nhs.net>
2024-12-19 10:25:23 +0000

    add context search param to doc ref

--- healthlake/scripts/searchDocRefAWS.sh
+++ healthlake/scripts/searchDocRefAWS.sh
@@ -6,4 +6,7 @@ curl -X GET "${HEALTHLAKEENDPOINT}DocumentReference" \
     -G --data-urlencode 'subject%3Aidentifier=https://fhir.nhs.uk/Id/nhs-number|6700028191' \
     -G --data-urlencode 'type=http://snomed.info/sct|736253002' \
     -G --data-urlencode 'custodian%3Aidentifier=https://fhir.nhs.uk/Id/ods-organization-code|Y05868' \
+    -G --data-urlencode 'event=http://snomed.info/sct|394848005,http://snomed.info/sct|394848005x' \
     -H "Authorization: Bearer THISISTHETOKEN"
+
+#    -G --data-urlencode 'event=http://snomed.info/sct|394848005' \
--- healthlake/scripts/updateDocRefConditionalAWS.sh
+++ healthlake/scripts/updateDocRefConditionalAWS.sh
@@ -2,7 +2,7 @@
 
 . ./setHealthlakeEndpoint.sh
 
-curl -X PUT "${HEALTHLAKEENDPOINT}DocumentReference?_id=033f9fcd-f74d-48ba-b9e9-84ee919af77b&custodian%3Aidentifier=https%3A%2F%2Ffhir.nhs.uk%2FId%2Fods-organization-code%7CxxxY05868" \
+curl -X PUT "${HEALTHLAKEENDPOINT}DocumentReference?_id=033f9fcd-f74d-48ba-b9e9-84ee919af77b&custodian%3Aidentifier=https%3A%2F%2Ffhir.nhs.uk%2FId%2Fods-organization-code%7CY05868" \
     -H "Content-Type: application/fhir+json" \
     -H "Authorization: Bearer THISISTHETOKEN" \
     --verbose --data "@docrefupdate.json" 
